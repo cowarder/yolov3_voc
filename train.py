a856326e987536ba57c66c40bd58d9efeccfeac8
cowarder <1615482151@qq.com>
2019-04-09 20:57:34 +0800

    add test module

--- train.py
+++ train.py
@@ -14,18 +14,50 @@ import argparse
 from yolo_voc.darknet import Darknet
 from utils import *
 
+use_cuda = True if torch.cuda.is_available() else False
+device = 'cpu'  # ''cuda' if use_cuda else 'cpu'
+
 FLAGS = None
-global loss_layers
+loss_layers = None
+model = None
+
+test_loader = None
+conf_thresh = 0.25
+nms_thresh = 0.4
+
+
+def test(batch_idx, conf_thresh):
+    model.eval()
+    net_w = model.width
+    net_h = model.height
+    nC = model.num_classes
+    with torch.no_grad:
+        for imgs, labels, org_w, org_h in test_loader:
+            imgs = imgs.to(device)
+            labels = labels.to(device)
+
+            result = model(imgs)
+            all_boxes = get_all_boxes(result, (net_w, net_h), conf_thresh, device)
+
+            for i in range(len(all_boxes)):
+                boxes = all_boxes[i]
+                boxes = nms(boxes, nms_thresh=nms_thresh)
+                num_pred = len(boxes)
+
+                if num_pred == 0:
+                    continue
+
+
 
 def main():
+    global loss_layers
+    global test_loader
     data_options = read_data_file(FLAGS.data)
     net_options = parse_cfg(FLAGS.config)[0]
 
-    use_cuda = True if torch.cuda.is_available() else False
-    device = 'cpu' # ''cuda' if use_cuda else 'cpu'
 
-    train_data = data_options['train']
-    test_data = data_options['valid']
+    train = data_options['train']
+    test = data_options['valid']
     names = data_options['names']
 
     batch_size = int(net_options['batch'])
@@ -45,15 +77,19 @@ def main():
         torch.cuda.manual_seed(0)
 
     model = model.to(device)
+    model.load_weights(weightfile="data/model.weights")
     loss_layers = model.loss_layers
     optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
 
-    train_data = dataset.YoloDataset(train_data, (model.width, model.height),
+    train_data = dataset.YoloDataset(train, (model.width, model.height),
                                      transform=transforms.ToTensor(), train=True)
+    test_data = dataset.YoloDataset(test, (model.width, model.height),
+                                     transform=transforms.ToTensor(), train=False)
     train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
     for epoch in range(epochs):
         for idx, (images, labels) in enumerate(train_loader):
-            #print(idx, images.shape, labels.shape)
+            # print(idx, images.shape, labels.shape)
             images = images.to(device)
             labels = labels.to(device)
 
@@ -62,15 +98,19 @@ def main():
             org_loss = []
             for i, l in enumerate(loss_layers):
                 l.seen += labels.data.size(0)
-                ol = l(output[i], labels)
+                ol = l(output[i]['output'], labels)
                 org_loss.append(ol)
             sum(org_loss).backward()
             optimizer.step()
+            if (idx+1) % 250 == 0:
+                model.save_weights('data/model.weights')
+                # print('Model saved.')
+                model.eval()
+                #with torch.no_grad:
 
+        model.save_weights('data/model.weights')
+        print('Model saved.')
 
-            #print(org_loss)
-            #print(output[0].shape)
-            #print(labels.shape)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
